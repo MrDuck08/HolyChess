a2b681906f15639a306e72a7eca67075f8b95a2f
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-09-22 08:18:12 +0200

    Shop Shop

--- Assets/Scripts/Shops.cs
+++ Assets/Scripts/Shops.cs
@@ -20,35 +20,46 @@ public class Shops : MonoBehaviour
 
     #region Upgrades
 
-    [SerializeField] List<GameObject> itemsInShop = new List<GameObject>();
+    [Header("Upgrades")]
+
+    [SerializeField] List<Transform> itemsInShopPosition = new List<Transform>();
+    [SerializeField] List<GameObject> upgradesObjectsTotal = new List<GameObject>();
+
+    [Header("Types Of Upgrades")]
+
+    #region General
+
+    [SerializeField] List<GameObject> generalUpgradeObjects = new List<GameObject>();
+
+    #endregion
 
     #region Pawn
 
-    int howManyUpgradesPawn;
+    [SerializeField] List<GameObject> pawnUpgradeObjects = new List<GameObject>();
 
     #endregion
 
     #region Horse
 
-    int howManyUpgradesHorse;
+    [SerializeField] List<GameObject> horseUpgradeObjects = new List<GameObject>();
 
     #endregion
 
     #region Tower
 
-    int howManyUpgradesTower;
+    [SerializeField] List<GameObject> towerUpgradeObjects = new List<GameObject>();
 
     #endregion
 
     #region Bishop
 
-    int howManyUpgradesBishop;
+    [SerializeField] List<GameObject> bishopUpgradeObjects = new List<GameObject>();
 
     #endregion
 
     #region Queen
 
-    int howManyUpgradesQueen;
+    [SerializeField] List<GameObject> queenUpgradeObjects = new List<GameObject>();
 
     #endregion
 
@@ -58,18 +69,85 @@ public class Shops : MonoBehaviour
     void Start()
     {
 
-        for (int i = 0; i < itemsInShop.Count; i++)
+        upgradesObjectsTotal.AddRange(generalUpgradeObjects);
+        upgradesObjectsTotal.AddRange(pawnUpgradeObjects);
+        upgradesObjectsTotal.AddRange(horseUpgradeObjects);
+        upgradesObjectsTotal.AddRange(towerUpgradeObjects);
+        upgradesObjectsTotal.AddRange(bishopUpgradeObjects);
+        upgradesObjectsTotal.AddRange(queenUpgradeObjects);
+
+        for (int i = 0; i < itemsInShopPosition.Count; i++)
         {
 
+            if(i <= 1)
+            {
+                Debug.Log("Basic Upgrade");
+
+                int whatUpgradeToChoce = Random.Range(0, upgradesObjectsTotal.Count);
+
+                upgradesObjectsTotal[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                upgradesObjectsTotal[whatUpgradeToChoce].SetActive(true);
+            }
+            else
+            {
+                Debug.Log("Special Upgrade");
+
+                #region Who To Upgrade
+
+                if (iAmBasicShop)
+                {
+                    int whatUpgradeToChoce = Random.Range(0, upgradesObjectsTotal.Count);
+
+                    upgradesObjectsTotal[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                    upgradesObjectsTotal[whatUpgradeToChoce].SetActive(true);
+                }
+
+                if (iAmPawnShop)
+                {
+                    int whatUpgradeToChoce = Random.Range(0, pawnUpgradeObjects.Count);
+
+                    pawnUpgradeObjects[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                    pawnUpgradeObjects[whatUpgradeToChoce].SetActive(true);
+                }
+
+                if (iAmTowerShop)
+                {
+                    int whatUpgradeToChoce = Random.Range(0, towerUpgradeObjects.Count);
+
+                    towerUpgradeObjects[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                    towerUpgradeObjects[whatUpgradeToChoce].SetActive(true);
+                }
+
+                if (iAmBishopShop)
+                {
+                    int whatUpgradeToChoce = Random.Range(0, bishopUpgradeObjects.Count);
+
+                    bishopUpgradeObjects[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                    bishopUpgradeObjects[whatUpgradeToChoce].SetActive(true);
+                }
+
+                if (iAmQueenShop)
+                {
+                    int whatUpgradeToChoce = Random.Range(0, queenUpgradeObjects.Count);
+
+                    queenUpgradeObjects[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                    queenUpgradeObjects[whatUpgradeToChoce].SetActive(true);
+                }
+
+                if (iAmHorseShop)
+                {
+                    int whatUpgradeToChoce = Random.Range(0, horseUpgradeObjects.Count);
+
+                    horseUpgradeObjects[whatUpgradeToChoce].transform.position = itemsInShopPosition[i].transform.position;
+                    horseUpgradeObjects[whatUpgradeToChoce].SetActive(true);
+                }
+
+                #endregion
+            }
 
 
         }
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
